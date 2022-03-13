a3e61206b3b1e66fa06cb0f40307b0d69ed699aa
Andres Munoz <andremude@hotmail.com>
2022-03-13 18:28:27 -0500

    react api

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = ({ query, setQuery }) => {
   return (
     <header className="navbar">
-      <div><h1 className='title'> Restaurants Finder 🔍</h1></div>
+      <div><h1 className='title'> Restaurant Finder 🔍</h1></div>
         <div className="links">
           <h4><a href="#">About Us</a></h4>
           <h4><a href="#">Contact</a></h4>
