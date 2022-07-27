2a07decbd306a0b2d4709792ada7553bd73e92c8
Andres Munoz <andremude@hotmail.com>
2022-07-27 16:51:22 -0500

    id removed

--- src/components/Restaurants.jsx
+++ src/components/Restaurants.jsx
@@ -84,7 +84,7 @@ const Restaurants = () => {
         <Table sx={{ minWidth:650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell style={tablestyle}>ID</TableCell>
+              {/* <TableCell style={tablestyle}>ID</TableCell> */}
               <TableCell style={tablestyle}>Name</TableCell>
               <TableCell style={tablestyle}>Address</TableCell>
               <TableCell></TableCell>
@@ -97,19 +97,19 @@ const Restaurants = () => {
           <TableBody>
             {data.map((item, i) => (
             <TableRow>
-              <TableCell style={tablestyle}> {item.id} </TableCell>
+              {/* <TableCell style={tablestyle}> {item.id} </TableCell> */}
               <TableCell> {item.name}  </TableCell>
               <TableCell> {item.address} </TableCell>
               <TableCell onClick={() => handleShow(item)} className="see-more"> <Show/> </TableCell>
               <TableCell
                 onClick={() => handleEdit(item)}>
-                  <svg width="18px" height="18px" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <svg width="18px" height="18px" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                   </svg>
               </TableCell>
               <TableCell
                 onClick={() => handleDelete(item)}>
-                  <svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                   </svg>
               </TableCell>
