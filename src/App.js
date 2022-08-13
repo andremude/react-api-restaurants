0792aad2cb4f338459ba0861f61bbe2b32af1656
Andres Munoz <andremude@hotmail.com>
2022-08-12 22:46:09 -0500

    auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Restaurants from './components/Restaurants'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 function App(inputText) {
   return (
     <div className="App">
       <Header />
-      <Restaurants />
+      {/* <Restaurants /> */}
+      <Routes>
+        <Route path="/" element={<Restaurants/>}/>
+        <Route path="/sign_in" element={<SignIn />} />
+        <Route path="/sign_up" element={<SignUp />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import { React } from "react";
 import '../styles/Header.css'
+import { Link } from "react-router-dom";
 
 const Header = ({ query, setQuery }) => {
 
   return (
     <header className="navbar">
       <h1 className='title'> Restaurant Finder 🔍</h1>
+      <Link to="/"><b>Home</b></Link>
+      <Link to="/sign_in"><b>Sign In</b></Link>
+      <Link to="/sign_up"><b>Sign Up</b></Link>
     </header>
   );
 };
